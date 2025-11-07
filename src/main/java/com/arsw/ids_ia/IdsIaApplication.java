af2bc3230e1b8782f442a3924240bea840359552
Esteban Aguilera <estebanac0903@gmail.com>
2025-11-07 12:38:16 -0500

    feat: enable scheduling for duration update AB#74

--- src/main/java/com/arsw/ids_ia/IdsIaApplication.java
+++ src/main/java/com/arsw/ids_ia/IdsIaApplication.java
@@ -2,8 +2,10 @@ package com.arsw.ids_ia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IdsIaApplication {
 
 	public static void main(String[] args) {
