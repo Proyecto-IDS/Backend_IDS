21b6a9b34e8614a5640c65cc9f067824dc4ac8ed
Esteban Aguilera <estebanac0903@gmail.com>
2025-11-27 22:55:00 -0500

    test: add unit/integration tests for War Room chat AB#117

--- src/test/java/com/arsw/ids_ia/IdsIaApplicationTests.java
+++ src/test/java/com/arsw/ids_ia/IdsIaApplicationTests.java
@@ -1,13 +1,9 @@
 package com.arsw.ids_ia;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-@Disabled("Integration test - requires database and full Spring context")
 class IdsIaApplicationTests {
 
 	@Test
